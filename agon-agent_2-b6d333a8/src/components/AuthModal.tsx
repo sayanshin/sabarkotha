96a379e563a59c681cb415fd10d6d7d988e93cb5
Sayan Chatterjee <sayanchatterjee.iitjee@gmail.com>
2026-09-09 21:32:56 +0530

    Update AuthModal.tsx

--- agon-agent_2-b6d333a8/src/components/AuthModal.tsx
+++ agon-agent_2-b6d333a8/src/components/AuthModal.tsx
@@ -127,7 +127,7 @@ export default function AuthModal({ open, onClose, onAdminSuccess }: AuthModalPr
 
     try {
       // Authenticate directly with Firebase
-      await signInWithEmailAndPassword(auth, 'sayanchatterjee.sonali@gmail.com', adminPw);
+      await signInWithEmailAndPassword(auth, 'rajib1975.chatterjee@gmail.com', adminPw);
       setAdminPw('');
       onClose();
       onAdminSuccess();
